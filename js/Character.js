7d30da992d195fb1e9e3e66e2c9c083b49bff3cc
remirobert <remi.robert@epitech.eu>
2014-10-14 16:15:11 +0200

    add features class character

--- js/Character.js
+++ js/Character.js
@@ -1,16 +1,33 @@
 function Character() {
+    this.isDead;
+    this.sexe;
     this.hair;
     this.hat;
     this.eye;
+    this.mask;
+    this.beard;
+    this.mustache;
 }
 
 function initCharacter() {
     var dataJson = loadJSON("ressource/data/character.json");
     var parse = dataJson.characters[Math.floor((Math.random() * 3))];
-    
+
     var selectedCharacter = new Character();
-    selectedCharacter.hair = parse.hair;
-    selectedCharacter.hat = parse.hat;
-    selectedCharacter.eye = parse.eye;
+    selectedCharacter.isDead = false;
+    selectedCharacter.hair = parse.attribut[0]["hair"];
+    selectedCharacter.hat = parse.attribut[0]["hat"];
+    selectedCharacter.eye = parse.attribut[0]["eye"];
+    selectedCharacter.mask = parse.attribut[0]["mask"];
+    selectedCharacter.beard = parse.attribut[0]["beard"];
+    selectedCharacter.mustache = parse.attribut[0]["mustache"];
+
+    if (parse.sexe == 0) {
+	selectedCharacter.sexe = "He ";
+    }
+    else {
+	selectedCharacter.sexe = "She ";
+    }
+    log(selectedCharacter);
     return selectedCharacter;
 }
